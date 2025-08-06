9f6acbe11b2c1b9dc92f9d172a32055ff62df104
babloo-tech <babloo.career@gmail.com>
2025-08-06 18:54:20 +0530

    updated code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
     <div>
       <BrowserRouter>
        <Link to={'/'} className='text-decoration-none'><header className='bg-primary  p-3 text-center position-sticky top-0  z-1'>
-          <h2 id='main-head' style={{padding:'23px'}} className=' text-white fw-bold text-decoration-none bi bi-person-video '> Online Video Application</h2>
+          <h2 id='main-head' style={{padding:'22px'}} className=' text-white fw-bold text-decoration-none bi bi-person-video '> Online Video Application</h2>
        </header></Link>
        <section>
            <Routes>
