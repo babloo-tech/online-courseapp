3d5417e1b61f7c9864bba50f91ed47c185226279
babloo-tech <babloo.career@gmail.com>
2025-08-30 17:38:56 +0530

    change code

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL:string="http://127.0.0.1:5050"
+export const BACKEND_URL:string= "https://course-app-nn6j.onrender.com"
 // "http://127.0.0.1:5050"
 // "https://course-app-nn6j.onrender.com"
 
